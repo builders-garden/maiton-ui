d4bc5dcdb192c8b7282202cf8ba60e9e3bcf155a
bianc8.eth <48656276+bianc8@users.noreply.github.com>
2024-10-02 17:06:18 +0200

    added code groups to install components

---
+++ vocs/vocs.config.tsx
@@ -0,0 +1,60 @@
+import * as React from "react";
+import { type SidebarItem, defineConfig } from "vocs";
+
+const sidebar: SidebarItem[] = [
+  {
+    text: "Getting Started",
+    link: "/getting-started",
+  },
+  {
+    text: "Components",
+    collapsed: false,
+    items: [
+      {
+        text: "Address",
+        link: "/components/address",
+      },
+      {
+        text: "Avatar",
+        link: "/components/avatar",
+      },
+      {
+        text: "Badge",
+        link: "/components/badge",
+      },
+      {
+        text: "Banner",
+        link: "/components/banner",
+      },
+      {
+        text: "Container",
+        link: "/components/container",
+      },
+      {
+        text: "Text",
+        link: "/components/text",
+      },
+      {
+        text: "TransactionResult",
+        link: "/components/transaction-result",
+      },
+      {
+        text: "UserBanner",
+        link: "/components/user-banner",
+      },
+    ],
+  },
+];
+
+export default defineConfig({
+  title: "Maiton UI",
+  description: "Maiton UI is a UI library for building easy-to-use frames.",
+  logoUrl: "/maiton-rounded.png",
+  iconUrl: "/favicon.png",
+  rootDir: "docs",
+  baseUrl:
+    process.env.VERCEL_ENV === "production"
+      ? "https://maiton.dev"
+      : "http://localhost:5173",
+  sidebar,
+});
